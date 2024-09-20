ae8893c965ceb9a530c5a77b8898305d76445f13
mraphaely <raphaelymaryana@gmail.com>
2024-09-20 18:30:12 -0300

    add projeto 02

--- Projeto-01/frontend/src/styles/Container.js
+++ Projeto-01/frontend/src/styles/Container.js
@@ -7,5 +7,5 @@ export const ContainerPrinc = styled.body`
     padding: 0;
     margin: 0 ;
     height: 100vh;
-    background: url(https://static.vecteezy.com/system/resources/previews/010/952/057/non_2x/black-gold-luxury-background-black-elegant-background-free-vector.jpg);
+    background: black;
 `
